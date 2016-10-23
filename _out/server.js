90782b66bc8d5c15b5b00608c739a05b2624b267
Ehab <ehabqadah@gmail.com>
2016-10-23 16:55:29 +0200

    added error handler for express server

--- _out/server.js
+++ _out/server.js
@@ -1,13 +1,20 @@
 var express = require('express'),
 app = express(),
+http = require('http'),
 server;
 
 app.use(express.static('static'));
 
+server = http.createServer(app);
 
-server=app.listen(process.env.port || 3000 ,function () {
-	var host = server.address().address;
-	var port = server.address().port;
+server.on('error', function (error) {
+ 	console.log(error);
+});
 
-	console.log("Example app listening at http://%s:%s", host, port);
+server.listen(process.env.port || 3000 ,function () {
+	
+	var host = server.address().address,
+	 port = server.address().port;
+
+	console.log("Server listening at http://%s:%s", host, port);
 });
